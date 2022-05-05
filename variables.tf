42e5e13867528aae33ef90d0fb22292ed0839e85
Chris Freeman <chris.freeman@digital.homeoffice.gov.uk>
2022-05-05 14:52:29 +0100

    format

--- variables.tf
+++ variables.tf
@@ -1,49 +1,49 @@
 variable "cluster_name" {
-    type = string
+  type = string
 }
 
 variable "namespace_name" {
-    type = string
+  type = string
 }
 
 variable "statefulset_name" {
-    type = string
+  type = string
 }
 
 variable "default_pager_notification_channels" {
-    type = list(number)
+  type = list(number)
 }
 
 variable "default_warning_notification_channels" {
-    type = list(number)
+  type = list(number)
 }
 
 variable "alert_pod_not_ready" {
-    type = list(object({
-      suffix                = string
-      trigger_after_minutes = number
-      channels              = list(number)
-      threshold    = string
-    }))
-    default = []
+  type = list(object({
+    suffix                = string
+    trigger_after_minutes = number
+    channels              = list(number)
+    threshold             = string
+  }))
+  default = []
 }
 
 variable "alert_pod_not_ready_default_disabled" {
-  type = bool
+  type    = bool
   default = false
 }
 
 variable "alert_logstash_up" {
-    type = list(object({
-      suffix                = string
-      trigger_after_minutes = number
-      channels              = list(number)
-      threshold    = string
-    }))
-    default = []
+  type = list(object({
+    suffix                = string
+    trigger_after_minutes = number
+    channels              = list(number)
+    threshold             = string
+  }))
+  default = []
 }
 
 variable "alert_logstash_up_default_disabled" {
-  type = bool
+  type    = bool
   default = false
 }
